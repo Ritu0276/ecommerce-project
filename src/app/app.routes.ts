8294e9d3a28c90d7ab25758e36af1da7b75460d2
Ritu47 <Ritu47@users.noreply.github.com>
2025-06-03 16:43:05 +0530

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Home } from './home/home';
+import { SellerAuth } from './seller-auth/seller-auth';
+
+export const routes: Routes = [
+
+   { path: '', component: Home },
+    { path: 'seller-auth', component: SellerAuth },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterModule, Header],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'ecomm-project';
+}
+
